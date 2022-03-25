38fd10b00a7b6cc73a2a3893771a69aa0efd127d
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2022-03-25 11:18:39 -0600

    Move old projects to https://github.com/BYU-PCCL/footron-projects

--- containers/cuda_cudnn_pytorch_pygame_jax/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t pccl/cuda_cudnn_pytorch_pygame_jax:0.1 .
-docker push pccl/cuda_cudnn_pytorch_pygame_jax:0.1
--- style_transfer/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t pccl/app_styletransfer:0.2 .
-docker push pccl/app_styletransfer:0.2
